89ebc3478efd5c73b65d2ca6d6929ffbc7b7c17a
tomekcegielkamobile <tomcegielka@gmail.com>
2019-05-12 12:11:04 +0200

    up

--- exercises/cli/utils.js
+++ exercises/cli/utils.js
@@ -2,7 +2,7 @@ const fs = require('fs')
 const path = require('path')
 
 // this path needs to be relative to work with fs
-const contactsLocation = 'contacts.json'
+const contactsLocation = path.join(path.dirname('utils'), '/contacts.json')
 
 /**
  * should read the contacts at the
@@ -10,7 +10,11 @@ const contactsLocation = 'contacts.json'
  * it to a js object
  */
 const getContacts = () => {
-  
+  console.log('contactsLocation')
+  // console.log(contactsLocation)
+  const file = fs.readFileSync('./contacts.json', {encoding: 'utf-8'})
+  console.log(file);
+    return {firstName: 'Tomek', lastName : 'ceg', phoneNumber: '726331041'}
 }
 
 /**
@@ -19,7 +23,9 @@ const getContacts = () => {
  * @param {Object} contacts contacts object
  */
 const saveContacts = (contacts) => {
-
+  console.log('save file')
+  console.log(contacts)
+  fs.writeFileSync(contactsLocation, contacts)
 }
 
 module.exports = {
